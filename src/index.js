9a0042cd245340257de6bed35f6409f20c0c3518
SaranyaMohan1990 <144958996+SaranyaMohan1990@users.noreply.github.com>
2024-03-01 17:39:46 -0800

    added basic form details in javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateRecepie(event) {
+  event.preventDefault();
+  let smoothieElement = document.querySelector("#smoothie");
 
+  new Typewriter("#smoothie", {
+    strings:
+      "This green smoothie is deliciously sweet, slightly tangy, and so good for you",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+let smoothieRecepieElement = document.querySelector("#smoothie-recepie-form");
+
+smoothieRecepieElement.addEventListener("submit", generateRecepie);
