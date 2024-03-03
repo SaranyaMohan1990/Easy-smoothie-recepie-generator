60e27f8ed162a11fe0017667a56a01d2f09cc742
SaranyaMohan1990 <144958996+SaranyaMohan1990@users.noreply.github.com>
2024-03-02 19:14:00 -0800

    edited basic ntml

--- src/index.js
+++ src/index.js
@@ -1,15 +1,22 @@
-function generateRecepie(event) {
-  event.preventDefault();
-  let smoothieElement = document.querySelector("#smoothie");
-
-  new Typewriter("#smoothie", {
-    strings:
-      "This green smoothie is deliciously sweet, slightly tangy, and so good for you",
+function showPoem(response) {
+  new Typewriter("#haiku", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
-let smoothieRecepieElement = document.querySelector("#smoothie-recepie-form");
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsElement = document.querySelector("#haiku-instruction");
+  let haikuElement = document.querySelector("#haiku");
+  let apikey = "14t6c91085afoc51fbb78bec011a39b3";
+  let prompt = ` User Instructions:generate a haiku poem on ${instructionsElement.value}`;
+  let context =
+    "you are a good haiku poet and your mission is to generate a haiku poem in basic HTML.";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apikey}`;
+  axios.get(apiUrl).then(showPoem);
+}
+let haikuPoemElement = document.querySelector("#haiku-poem-form");
 
-smoothieRecepieElement.addEventListener("submit", generateRecepie);
+haikuPoemElement.addEventListener("submit", generatePoem);
